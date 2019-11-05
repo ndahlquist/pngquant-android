f7a5071ace7e8ad35b61c7cde582530ae2408142
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2019-11-05 01:24:53 -0800

    Modernize main activity. (#13)

--- testapp/src/main/java/com/nicdahlquist/pngquant/testapp/MainActivity.java
+++ testapp/src/main/java/com/nicdahlquist/pngquant/testapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.nicdahlquist.pngquant.testapp;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,29 +15,26 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.textView);
+        final TextView textView = findViewById(R.id.textView);
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Processing...");
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            textView.setText("Processing...");
 
-                new AsyncTask<Void, Void, Long>() {
-                    @Override
-                    protected Long doInBackground(Void... params) {
-                        long startMillis = System.currentTimeMillis();
-                        mPngQuantTest.testPngQuant(MainActivity.this);
-                        long endMillis = System.currentTimeMillis();
-                        return endMillis - startMillis;
-                    }
+            new AsyncTask<Void, Void, Long>() {
+                @Override
+                protected Long doInBackground(Void... params) {
+                    long startMillis = System.currentTimeMillis();
+                    mPngQuantTest.testPngQuant(MainActivity.this);
+                    long endMillis = System.currentTimeMillis();
+                    return endMillis - startMillis;
+                }
 
-                    @Override
-                    protected void onPostExecute(Long millis) {
-                        textView.setText("Processing took " + millis + " millis.");
-                    }
-                }.execute();
-            }
+                @Override
+                protected void onPostExecute(Long millis) {
+                    textView.setText("Processing took " + millis + " millis.");
+                }
+            }.execute();
         });
     }
 }
