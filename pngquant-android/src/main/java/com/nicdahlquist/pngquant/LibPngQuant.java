4a9ae87cfda3e0349649ac6fed5b70faf08b19e2
SerpentSpirale <serpentspirale@gmail.com>
2021-04-05 12:56:13 +0200

    Add dithering control | clean up bad code

--- pngquant-android/src/main/java/com/nicdahlquist/pngquant/LibPngQuant.java
+++ pngquant-android/src/main/java/com/nicdahlquist/pngquant/LibPngQuant.java
@@ -5,35 +5,21 @@ import java.io.File;
 public class LibPngQuant {
 
     public boolean pngQuantFile(File inputFile, File outputFile) {
-        if (inputFile == null) throw new NullPointerException();
-        if (!inputFile.exists()) throw new IllegalArgumentException();
-        if (outputFile == null) throw new NullPointerException();
-        if (outputFile.length() != 0) throw new IllegalArgumentException();
-
-        String inputFilename = inputFile.getAbsolutePath();
-        String outputFilename = outputFile.getAbsolutePath();
-
-        //Using default quality from the pngquant windows bat file
-        return nativePngQuantFile(inputFilename, outputFilename, 50, 100, 1);
+        //Use default quality in the windows batch file bundled with pngQuant
+        return pngQuantFile(inputFile, outputFile,50,100);
     }
 
     public boolean pngQuantFile(File inputFile, File outputFile, int minQuality, int maxQuality) {
-        if (inputFile == null) throw new NullPointerException();
-        if (!inputFile.exists()) throw new IllegalArgumentException();
-        if (outputFile == null) throw new NullPointerException();
-        if (outputFile.length() != 0) throw new IllegalArgumentException();
-        if (maxQuality < 0 || maxQuality > 100) throw new IllegalArgumentException();
-        if (minQuality < 0 || minQuality > 100) throw new IllegalArgumentException();
-        if (maxQuality < minQuality) throw new IllegalArgumentException();
-
-        String inputFilename = inputFile.getAbsolutePath();
-        String outputFilename = outputFile.getAbsolutePath();
-
         //Use lowest speed to get the best quality
-        return nativePngQuantFile(inputFilename, outputFilename, minQuality, maxQuality, 1);
+        return pngQuantFile(inputFile, outputFile, minQuality, maxQuality, 1);
     }
 
     public boolean pngQuantFile(File inputFile, File outputFile, int minQuality, int maxQuality, int speed) {
+        //Use the default dither value.
+        return pngQuantFile(inputFile, outputFile, minQuality, maxQuality, speed, 1f);
+    }
+
+    public boolean pngQuantFile(File inputFile, File outputFile, int minQuality, int maxQuality, int speed, float floydDitherAmount) {
         if (inputFile == null) throw new NullPointerException();
         if (!inputFile.exists()) throw new IllegalArgumentException();
         if (outputFile == null) throw new NullPointerException();
@@ -42,17 +28,18 @@ public class LibPngQuant {
         if (minQuality < 0 || minQuality > 100) throw new IllegalArgumentException();
         if (maxQuality < minQuality) throw new IllegalArgumentException();
         if (speed < 1 || speed > 11) throw new IllegalArgumentException();
+        if (floydDitherAmount < 0f || floydDitherAmount > 1f) throw new IllegalArgumentException();
 
         String inputFilename = inputFile.getAbsolutePath();
         String outputFilename = outputFile.getAbsolutePath();
 
-        return nativePngQuantFile(inputFilename, outputFilename, minQuality, maxQuality, speed);
+        return nativePngQuantFile(inputFilename, outputFilename, minQuality, maxQuality, speed, floydDitherAmount);
     }
 
     static {
         System.loadLibrary("pngquantandroid");
     }
 
-    private static native boolean nativePngQuantFile(String inputFilename, String outputFilename, int minQuality, int maxQuality, int speed);
+    private static native boolean nativePngQuantFile(String inputFilename, String outputFilename, int minQuality, int maxQuality, int speed, float floydDitherAmount);
 
 }
